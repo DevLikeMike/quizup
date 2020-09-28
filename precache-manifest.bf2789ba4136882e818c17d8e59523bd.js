f746231ae1a585a333287850378bfb2b82ad2bfa
DevLikeMike <michael@MacBook-Pro.lan1>
2020-09-28 10:48:23 -0400

    Updates

---
+++ precache-manifest.bf2789ba4136882e818c17d8e59523bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106dae20c31132004a4d3fe44ffecc25",
+    "url": "/quizup/index.html"
+  },
+  {
+    "revision": "24c51511b68bc97f78fd",
+    "url": "/quizup/static/css/main.7069469d.chunk.css"
+  },
+  {
+    "revision": "7b0ee9b6ed2d129c34c8",
+    "url": "/quizup/static/js/2.b59e9f05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizup/static/js/2.b59e9f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c51511b68bc97f78fd",
+    "url": "/quizup/static/js/main.5ba66b37.chunk.js"
+  },
+  {
+    "revision": "a6da21eae4e638106b02",
+    "url": "/quizup/static/js/runtime-main.0a232946.js"
+  }
+]);
