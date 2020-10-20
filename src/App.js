423031f9be05f37a5aa3bb35ea905a75c60ad678
DevLikeMike <michael@MacBook-Pro.local>
2020-10-20 09:25:38 -0400

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 //Components
 import Welcome from "./components/pages/Welcome";
 import QuestionForm from "./components/pages/QuestionForm";
